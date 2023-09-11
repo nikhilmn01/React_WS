ee81a8c4340751bbb511c279e9705feebd7a5b46
Nikhil <nikhilmn01@gmail.com>
2023-09-11 19:26:37 +0530

    Investment calculator added

--- React_Hands-On/react-app-1/src/components/Expenses/Expenses.js
+++ React_Hands-On/react-app-1/src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -13,7 +14,6 @@ const Expenses = (props) => {
   };
 
   const filteredExpenses = props.items.filter(expense => {
-    console.log(`expense array ${JSON.stringify(expense)}`);
     return expense.date.getFullYear().toString() === filteredYear;
   })
   return (
@@ -22,14 +22,12 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesChart
+        expenses={filteredExpenses}
+      />
+      <ExpensesList
+        items={filteredExpenses}
+      />
     </Card>
   );
 }
--- React_Hands-On/react-app-1/src/components/NewExpense/NewExpense.js
+++ React_Hands-On/react-app-1/src/components/NewExpense/NewExpense.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
-  const handleSubmitExpenseForm = (expenseData) =>{
-    const enteredExpenseData = {...expenseData,id:Math.random().toString()}
+  const [showForm, setShowForm] = useState(false);
+  const handleSubmitExpenseForm = (expenseData) => {
+    const enteredExpenseData = { ...expenseData, id: Math.random().toString() }
     props.onSubmitExpenseForm(enteredExpenseData);
+  }
+
+  const handleAddExpense = () => {
+    setShowForm(true);
+  }
+
+  const handleCancel = () => {
+    setShowForm(false);
   }
   return (
     <div className='new-expense'>
-      <ExpenseForm onSubmitExpenseForm={handleSubmitExpenseForm}/>
+      {!showForm ?
+        <button onClick={handleAddExpense}>Add New Expense</button>
+        :
+        <ExpenseForm
+          onSubmitExpenseForm={handleSubmitExpenseForm}
+          onCancel={handleCancel}
+        />
+      }
     </div>
   );
 };
