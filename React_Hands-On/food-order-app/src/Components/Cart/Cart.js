acc1149084691fe206afaff34ffe0a2179939257
Nikhil <nikhilmn01@gmail.com>
2023-09-25 16:17:53 +0530

    Food Order App Complete

--- React_Hands-On/food-order-app/src/Components/Cart/Cart.js
+++ React_Hands-On/food-order-app/src/Components/Cart/Cart.js
@@ -1,10 +1,14 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout"
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
   const hasItems = cartCtx.items.length > 0;
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -17,6 +21,25 @@ const Cart = (props) => {
       amount: 1
     });
   };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch('https://react-hands-on-1997-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    });
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => {
@@ -34,19 +57,64 @@ const Cart = (props) => {
     </ul>
   );
 
-  return (
-    <Modal onHide={props.onHide}>
-      {cartItems}
-      <div className={classes.total}>
-        <span> Total Amount</span>
-        <span> {totalAmount} </span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onHide}>
-          Close
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onHide}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <React.Fragment>
+      {!isCheckout && cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onHide} />
+      )}
+      {!isCheckout && modalActions}
+    </React.Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const didSubmitModalContent = (
+    <React.Fragment>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+      <button className={classes.button} onClick={props.onHide}>
+        Close
+      </button>
+    </div>
+    </React.Fragment>
+  );
+
+  return (
+    // <Modal onHide={props.onHide}>
+    //   {cartItems}
+    //   <div className={classes.total}>
+    //     <span> Total Amount</span>
+    //     <span> {totalAmount} </span>
+    //   </div>
+    //   <div className={classes.actions}>
+    //     <button className={classes["button--alt"]} onClick={props.onHide}>
+    //       Close
+    //     </button>
+    //     {hasItems && <button className={classes.button}>Order</button>}
+    //   </div>
+    // </Modal>
+    <Modal onHide={props.onHide}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- React_Hands-On/food-order-app/src/Components/Meals/AvailableMeals.js
+++ React_Hands-On/food-order-app/src/Components/Meals/AvailableMeals.js
@@ -1,48 +1,68 @@
-import classes from './AvailableMeals.module.css';
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import { useEffect, useState } from "react";
 
 const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => {
-        return <MealItem key={meal.id} meal={meal}/>
-    })
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-hands-on-1997-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseJson = await response.json();
+      const mealsArray = [];
+      for (const key in responseJson) {
+        mealsArray.push({
+          id: key,
+          name: responseJson[key].name,
+          description: responseJson[key].description,
+          price: responseJson[key].price,
+        });
+        console.log(key, responseJson[key]);
+      }
+      setMeals(mealsArray);
+    };
+    fetchMeals();
+  }, []);
+  const mealsList = meals.map((meal) => {
+    return <MealItem key={meal.id} meal={meal} />;
+  });
 
-    return (
-        <section className={classes.meals}>
-            <Card>
-                <ul>
-                    {mealsList}
-                </ul>
-            </Card>
-        </section>
-    );
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
